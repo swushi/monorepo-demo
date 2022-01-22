a4f8e11b2172ad5a76dc32d6ded8116805e26953
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-22 16:03:39 -0600

    got env working

--- packages/test-package/dist/src/index.js
+++ packages/test-package/dist/src/index.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.greeting = void 0;
+// import dotenv from 'dotenv';
+// dotenv.config({path: '../../.env'})
+// console.log('hello', process.env.IDK)
 exports.greeting = 'hi idk';
--- packages/test-package/src/index.ts
+++ packages/test-package/src/index.ts
@@ -1,1 +1,4 @@
+// import dotenv from 'dotenv';
+// dotenv.config({path: '../../.env'})
+// console.log('hello', process.env.IDK)
 export const greeting: string = 'hi idk';
