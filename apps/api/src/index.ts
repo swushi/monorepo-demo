0e5126314f51a91f6947837658a98d2b5dce76b5
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-22 16:28:33 -0600

    setup api with env

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello');
+import express from "express";
+
+import { config } from "dotenv";
+config({ path: "../../.env" });
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.API_PORT || 8080
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
