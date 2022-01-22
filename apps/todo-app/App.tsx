72510a7102589ff81b90fafacbfd5cba21a30db0
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-22 16:03:32 -0600

    got env working in expo

--- apps/todo-app/App.tsx
+++ apps/todo-app/App.tsx
@@ -2,6 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { greeting } from 'test-package';
 
+// @ts-ignore 
+import { IDK, WHAT, PLEASE } from '@env';
+
+console.log(IDK, WHAT, PLEASE)
+
 export default function App() {
   return (
     <View style={styles.container}>
