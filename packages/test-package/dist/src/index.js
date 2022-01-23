4fcfab2548760533910dae1f72f87803313feb1c
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-22 23:18:16 -0600

    remove

--- packages/test-package/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greeting = void 0;
-// import dotenv from 'dotenv';
-// dotenv.config({path: '../../.env'})
-// console.log('hello', process.env.IDK)
-exports.greeting = 'hi idk';
