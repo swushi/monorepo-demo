1c0b1871116f9a06cef98565b732ba730881d3f9
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-22 23:30:37 -0600

    configure package.json

--- packages/test-package/src/index.ts
+++ packages/test-package/src/index.ts
@@ -1,4 +1,4 @@
 // import dotenv from 'dotenv';
 // dotenv.config({path: '../../.env'})
 // console.log('hello', process.env.IDK)
-export const greeting: string = 'hi idk';
+export const greeting: string = 'hello';
